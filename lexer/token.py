fedc582e16a4055b7060728f5ff4f3f69f6a4147
LordBaryhobal <lordbaryhobal@gmail.com>
2026-05-13 19:17:55 +0200

    feat(parser): add base lexer class
    the lexer and token structures were adapted from another project (see docstring on the Lexer class)

---
+++ lexer/token.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any
+
+from lexer.position import Position
+
+
+class TokenType(Enum):
+    # Punctuation
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    COLON = auto()
+    COMMA = auto()
+    UNDERSCORE = auto()
+
+    # Operators
+    PLUS = auto()
+
+    # Literals
+    IDENTIFIER = auto()
+    NUMBER = auto()
+    TRUE = auto()
+    FALSE = auto()
+    NONE = auto()
+
+    # Misc
+    COMMENT = auto()
+    WHITESPACE = auto()
+    EOF = auto()
+    NEWLINE = auto()
+
+
+@dataclass(frozen=True)
+class Token:
+    """A scanned token"""
+    type: TokenType
+    lexeme: str
+    value: Any
+    position: Position
