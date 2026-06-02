63a43d79dd044a1126da2541d8d80171a33dd81c
LordBaryhobal <lordbaryhobal@gmail.com>
2026-06-02 13:07:53 +0200

    chore: update examples

--- examples/01_simple_type_checking/03_control_flow.py
+++ examples/01_simple_type_checking/03_control_flow.py
@@ -4,13 +4,20 @@ def minimum(x: int, y: int):
     else:
         return y
 
+
 a = 15
 b = 72
 c = minimum(a, b)
+
 
 def factorial(n: int) -> int:
     if n <= 1:
         return 1
     return n * factorial(n - 1)
 
+
 category = "Category 1" if a < 10 else "Category 2"
+
+
+def foo() -> None:
+    pass
