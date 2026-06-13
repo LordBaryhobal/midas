84a5f41e62030d855d1f06b8df1ea87f1045f6f9
LordBaryhobal <lordbaryhobal@gmail.com>
2026-06-13 12:40:26 +0200

    fix: extend example of complex types

--- examples/01_simple_type_checking/04_complex_types.py
+++ examples/01_simple_type_checking/04_complex_types.py
@@ -1,5 +1,6 @@
 # type: ignore
 # ruff: disable [F821]
+
 p1: Coordinate
 p2: Coordinate
 
@@ -9,6 +10,21 @@ diff_y = p2.y - p1.y
 dist = diff_x + diff_y
 
 p2.x += cast(Meter, 1)
-p2.y = True
-p2.z = 3
-p2.x.a = 3
+p2.y = True  # invalid, wrong type
+p2.z = 3  # invalid, no property 'z' on Coordinate
+p2.x.a = 3  # invalid, no properties on Meter
+
+foo: list[float] = []
+
+append = foo.append
+
+foo.append("")  # invalid, must be float
+foo.append(2)
+append(True)  # invalid, must be float
+append(2)
+
+bar: list[list[Meter]]
+
+bar.append([p2.x])
+
+foo2 = foo + foo
